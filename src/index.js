e1e06b3d0a8e1e96142f1387a835443df0b6965c
cgolinski <cgolinski@users.noreply.github.com>
2018-09-23 18:10:09 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
-const admin = require("firebase-admin");
+const express = require('express');
+const morgan = require('morgan');
+const admin = require('firebase-admin');
 
-const router = require("./api");
-const { logger } = require("./utils/logger");
-const { errorHandler } = require("./middleware/error-handler");
-const serviceAccount = require("../firebase-credentials.json");
+const router = require('./api');
+const { logger } = require('./utils/logger');
+const { errorHandler } = require('./middleware/error-handler');
+const serviceAccount = require('../firebase-credentials.json');
 
 // initialize firebase store
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
@@ -20,19 +20,15 @@ const app = express();
 // The port the express app will listen on
 const port = process.env.PORT || 8081;
 
-logger.info("🤖 Initializing middleware");
+logger.info('🤖 Initializing middleware');
 
-app.use(morgan("tiny", { stream: logger.stream }));
-app.use("/", router);
+app.use(morgan('tiny', { stream: logger.stream }));
+app.use('/', router);
 app.use(errorHandler);
 
 // Serve the application at the given port
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
     logger.info(`🎧 Listening at http://localhost:${port}/`);
   });
 }
-
-module.exports = {
-  app
-};
